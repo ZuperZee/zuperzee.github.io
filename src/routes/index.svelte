bf492a0eaf02540e9df258151102c2123f92e568
ZuperZee <zuperpenguin@gmail.com>
2020-10-18 02:36:33 +0200

    feat(index): replace homepage with zuperzee text transition

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,51 +1,27 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import ZuperZeeDraw, { visible } from "./../components/ZuperZeeDraw.svelte";
+
+  // Reset the variable when switching between pages
+  // visible.set(false);
+
+  onMount(() => {
+    visible.set(true);
+  });
+</script>
+
 <style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
+  @media (min-height: 500px) {
+    .spacer {
+      margin-bottom: 100px;
     }
   }
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>ZuperZee</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="spacer" />
 
-<figure>
-  <img alt="Success Kid" src="successkid.jpg" />
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p>
-  <strong>Try editing this file (src/routes/index.svelte) to test live
-    reloading.</strong>
-</p>
+<ZuperZeeDraw />
